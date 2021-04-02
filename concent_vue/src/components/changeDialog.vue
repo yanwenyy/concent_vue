4ebf4fef75910cb13fc0d201e4afdf4980ed3807
zw02 <wzjbk23561000>
2021-04-02 09:37:13 +0800

    分包选择列表只能选择审核通过的数据

--- concent_vue/src/components/changeDialog.vue
+++ concent_vue/src/components/changeDialog.vue
@@ -192,7 +192,7 @@
       init() {
         this.visible = true
         this.$http
-          .post('/api/statistics/StatisticsProject/list/loadPageData', this.searchform, { isLoading: false })
+          .post('/api/statistics/StatisticsProject/list/selectProjectFb', this.searchform, { isLoading: false })
           .then(res => {
             if (res.data && res.data.code === 200) {
               this.dataList = res.data.data.records
