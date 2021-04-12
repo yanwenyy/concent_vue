f99271e8cfebdcdf80b39c72d0cc9bea041a19d4
yanwenyy <18738390058>
2021-04-12 17:18:34 +0800

    修改工程承包,勘察设计合同输入框顺序

--- concent_vue/src/views/statistics/project/change/otherDetail.vue
+++ concent_vue/src/views/statistics/project/change/otherDetail.vue
@@ -1035,7 +1035,7 @@
               .then((res) => {
                 if (res.data.code === 200) {
                   this.$message({
-                    message: '保存成功',
+                    message:  `${type=='save'?'保存':'提交'}成功`,
                     type: 'success'
                   })
                   this.$router.push({
@@ -1043,7 +1043,7 @@
                   })
                 } else {
                   this.$message({
-                    message: '保存失败',
+                    message:  `${type=='save'?'保存':'提交'}失败`,
                     type: 'error'
                   })
                 }
