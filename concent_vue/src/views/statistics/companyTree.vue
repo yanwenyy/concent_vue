6901dd88bffbe9771e2ab127faa729834aa010c4
GaoDeXin <65593527+q809041405@users.noreply.github.com>
2021-07-31 15:19:09 +0800

    fix 工程承包项目 新增 承建单位 改为树

---
+++ concent_vue/src/views/statistics/companyTree.vue
@@ -0,0 +1,216 @@
+<template>
+  <el-dialog
+    title="选择单位"
+    :visible.sync="dialogVisible"
+    width="30%"
+  >
+    <div class="tree-search-input">
+      <el-input
+        placeholder="输入关键字进行过滤"
+        v-model="filterText">
+      </el-input>
+    </div>
+    <div class="tree-div" v-if="ifChek==true">
+      <!--:data="datas"-->
+      <el-tree
+        :filter-node-method="filterNode"
+        class="filter-tree"
+        :class="ifGFGS?'ifGFGS':''"
+        ref="tree"
+        show-checkbox
+        :check-strictly="true"
+        :load="loadNode"
+        lazy
+        :props="defaultProps"
+        :highlight-current="true"
+        node-key="code"
+        :default-checked-keys="list"
+        >
+      </el-tree>
+    </div>
+    <div class="tree-div" v-if="ifChek==false">
+      <!--:data="datas"-->
+      <el-tree
+        :filter-node-method="filterNode"
+        class="filter-tree"
+        ref="tree"
+        :load="loadNode"
+        lazy
+        :props="defaultProps"
+        :highlight-current="true"
+        node-key="code"
+        @node-click="handleNodeClick"
+      >
+      </el-tree>
+    </div>
+    <div style="text-align: center">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button @click="getCheckedNodes"  type="primary">确定</el-button>
+    </div>
+
+  </el-dialog>
+</template>
+
+<script>
+  import datas from '@/utils/position'
+  export default {
+    watch: {
+      filterText(val) {
+        this.$refs.tree.filter(val);
+      }
+    },
+    data() {
+      return {
+        ifGFGS:'',//是否可选择股份公司
+        filterText: '',
+        datas: [],
+        list:[],
+        dialogVisible:true,
+        ifChek:false,
+        userInfo:{},
+        defaultProps: {
+          children: 'children',
+          label: 'name',
+          // isLeaf: 'leaf'
+        },
+        notSelect:['0','1','01','02','03','04','05','06','101','102','103','105','106','108'],
+        name:'',
+        id:null,
+      };
+    },
+    mounted(){
+      //json方法引入数据
+      // this.$http.get('/static/jsonData/position.json', { isLoading: false }).then(res =>{
+      //   // console.log(res.data.data);
+      //   this.datas=res.data.data
+      // });
+
+      //js方法引入数据
+      this.datas=datas;
+
+      //获取用户信息
+      this.$http
+        .get(
+          '/jsonapi/System/system/v1.0/userinfo',
+        )
+        .then(res => {
+          this.userInfo= res.data.data;
+      })
+    },
+    methods: {
+      filterNode(value, data) {
+        if (!value) return true;
+        return data.name.indexOf(value) !== -1;
+      },
+      loadNode(node, resolve) {
+          if (node.level === 0) {
+            this.getFrist(resolve)
+          }
+        if (node.level >= 1) {
+          // 注意！把resolve传到你自己的异步中去
+          this.getChildren(node, resolve);
+        }
+      },
+      getFrist(resolve){
+        // this.$http
+        //   .get(
+        //     '/jsonapi/System/system/hr/orglist',
+        //   )
+        //   .then(res => {
+        //     var datas= res.data.data;
+        //     resolve(datas);
+        //  })
+
+        //假数据,解决不稳定,测试完得换
+        var datas=[{code: "0000100001",
+          fullname: "股份公司总部",
+          hookOrgName: "中国铁建股份有限公司",
+          id: 986436,
+          ifLastNode: "0",
+          ifUse: "1",
+          name: "股份公司",
+          order: 1,
+          path: "股份公司",
+          providerId: "hr",
+          relationOrgName: "股份公司",
+          show: true,
+          type: 1,
+          virtual: false}];
+        resolve(datas);
+      },
+      getChildren(node, resolve){
+        this.$http
+          .get(
+            '/jsonapi/System/system/supmanage/org/v1.0/tree/'+node.data.code+'/'+this.userInfo.currentPostCode+'?type=orgAuth',
+          )
+          .then(res => {
+              var datas= res.data.data;
+              resolve(datas);
+          })
+      },
+      init(type,list,ifChek,index,tableList){
+        //type使用组件的名称,
+        // list组织树默认勾选的id,
+        // ifChek是否可多选
+        //index table使用组件时的下标
+        //tableList tabel使用组件时的list
+        //ifGFGS  是否可选择股份公司
+        this.dialogVisible = true;
+        // this.ifChek=ifChek!=undefined?ifChek:true;
+        this.ifChek = false
+        this.type=type;
+        this.index=index;
+        this.tableList=tableList;
+        //可以选择股份公司的单位
+        if(type=='使用资质单位'){
+          this.ifGFGS=true;
+        }
+        if(list&&list!=''){
+          this.list=list.split(",")
+        }
+      },
+      handleNodeClick(data) {
+        // if(this.notSelect.indexOf(data.detailCode)=='-1'){
+        //   this.dialogVisible = false;
+        //   data.type=this.type;
+        //   data.index=this.index;
+        //   data.tableList=this.tableList;
+        //   this.$emit('refreshBD',data)
+        // }
+        this.name = data.name
+        this.id = data.id
+      },
+      getCheckedNodes() {
+        // var list=this.$refs.tree.getCheckedNodes();
+        // list.forEach((item)=>{
+        //   item.detailName=item.name;
+        //   item.id=item.code;
+        // })
+        // var data=list;
+        // data.type=this.type;
+        // data.index=this.index;
+        // data.tableList=this.tableList;
+        let data = []
+        data.name=this.name;
+        data.id=this.id;        
+        this.$emit('refreshBD',data)
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  /deep/ .el-tree > .el-tree-node > .el-tree-node__content .el-checkbox{
+
+    display: none;
+  }
+  /deep/ .ifGFGS > .el-tree-node > .el-tree-node__content .el-checkbox{
+
+    display: inline-block;
+  }
+  .tree-div{
+    height: 300px;
+    overflow: auto;
+    margin-bottom: 20px;
+  }
+</style>
