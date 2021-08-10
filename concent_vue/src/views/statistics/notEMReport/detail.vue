2fdd7e408ce6f4292af25d2bf2839bd963947d6e
GaoDeXin <65593527+q809041405@users.noreply.github.com>
2021-08-10 14:42:02 +0800

    fix 修改 工程填报 房地产板块 主要项目管理 计算方式

--- concent_vue/src/views/statistics/notEMReport/detail.vue
+++ concent_vue/src/views/statistics/notEMReport/detail.vue
@@ -1866,16 +1866,17 @@
                     width="150"
                   >
                     <template slot-scope="scope">
+                      <!-- :disabled="p.actpoint === 'look'||p.actpoint=='task'||scope.row.isEdit==-1" -->
                       <el-input
                         @input="isFloor(scope.row.income,scope.$index,detailform.fdc_list,'income'),getGyzzCz(detailform.fdc_list,detailform.sumByMon_3,'income')"
                         v-if="scope.row.country=='01'"
-                        :disabled="p.actpoint === 'look'||p.actpoint=='task'||scope.row.isEdit==-1"
+                        :disabled="true"
                         clearable
                         v-model="scope.row.income"/>
                       <el-input
                         @input="isFloor(scope.row.overseasIncome,scope.$index,detailform.fdc_list,'overseasIncome'),getGyzzCz(detailform.fdc_list,detailform.sumByMon_3,'overseasIncome')"
                         v-if="scope.row.country=='02'"
-                        :disabled="p.actpoint === 'look'||p.actpoint=='task'||scope.row.isEdit==-1"
+                        :disabled="true"
                         clearable
                         v-model="scope.row.overseasIncome"/>
                     </template>
@@ -3795,6 +3796,26 @@
           num=num+Number(item[name]||0);
         })
         obj[name]=num;
+        // 自动计算房地产板块的房地产营业收入
+        if (name === "inRevenue") {
+          // 执行修改方法
+          let num=0;
+          this.detailform.fdc_list.forEach((item)=>{
+            item.income = Number(item.inRevenue) + Number(item.offRevenue)
+          })
+          this.detailform.fdc_list.forEach((item)=>{
+            num=num+Number(item.income||0);
+          })
+          this.detailform.sumByMon_3.income=num;
+          console.info(this.detailform.fdc_list)
+          console.info(this.detailform.sumByMon_3)
+        }else if (name === "inRevenueHw") {
+          obj.overseasIncome = obj.inRevenueHw + obj.offRevenueHw
+        }else if (name === "offRevenue") {
+          obj.income = obj.inRevenue + obj.offRevenue
+        }else if (name === "offRevenueHw") {
+          obj.overseasIncome = obj.inRevenueHw + obj.offRevenueHw
+        }
         this.$forceUpdate();
       },
       //重置
