8a87ed55b8e4eb87e8608d5ac0abb10a2fee45c6
yanwenyy <18738390058>
2021-08-25 18:01:29 +0800

    bug修改

--- concent_vue/src/views/manage/contractManager/search/list.vue
+++ concent_vue/src/views/manage/contractManager/search/list.vue
@@ -1,28 +1,22 @@
 <template>
   <div class="searchListClass" style="margin-bottom: -50px;">
-    <el-menu default-active="2" class="el-menu-vertical-demo" >
-      <el-submenu index="1">
-        <template slot="title">
-          <span>查询条件</span>
-        </template>
-        <el-menu-item-group>
-      <el-form class="queryForm" :inline="true" :model="searchform" @keyup.enter.native="getData()">
-        <el-form-item label="填报日期开始:">
-          <el-date-picker
-            clearable
-            v-model="searchform.beginTime"
-            type="date"
-            value-format="timestamp">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="填报日期结束:">
-          <el-date-picker
-            clearable
-            v-model="searchform.stopTime"
-            type="date"
-            value-format="timestamp">
-          </el-date-picker>
-        </el-form-item>
+    <el-form class="queryForm" :inline="true" :model="searchform" @keyup.enter.native="getData()">
+      <el-form-item label="填报日期开始:">
+        <el-date-picker
+          clearable
+          v-model="searchform.beginTime"
+          type="date"
+          value-format="timestamp">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="填报日期结束:">
+        <el-date-picker
+          clearable
+          v-model="searchform.stopTime"
+          type="date"
+          value-format="timestamp">
+        </el-date-picker>
+      </el-form-item>
       <el-form-item label="合同类型:">
         <el-select
           clearable
@@ -40,7 +34,10 @@
         </el-select>
       </el-form-item>
       <el-form-item label="填报单位:">
-        <el-input v-model="searchform.createOrgName" placeholder="填报单位" clearable></el-input>
+        <!--<el-input v-model="searchform.createOrgName" placeholder="填报单位" clearable></el-input>-->
+        <el-input v-model="searchform.createOrgName" placeholder="填报单位" clearable @clear="searchform.createOrgName=''">
+          <el-button slot="append" icon="el-icon-search"  @click="addDw('填报单位',searchform.createOrgName,false)"></el-button>
+        </el-input>
       </el-form-item>
       <el-form-item label="合同名称:">
         <el-input v-model="searchform.contractName" placeholder="合同名称" clearable></el-input>
@@ -58,6 +55,7 @@
           @change="getTwoXZ"
           size="mini"
           v-model="searchform.projectNatureFirstId"
+          @clear="searchform.projectNatureFirstId='',searchform.projectNatureFirstName=''"
         >
           <el-option
             :key="index"
@@ -166,24 +164,24 @@
           <el-button slot="append" icon="el-icon-search"  @click="addDw('主推单位',searchform.contractMianOrg)"></el-button>
         </el-input>
       </el-form-item>
-      <el-form-item
-        label="是否变更:"
-      >
-        <el-select
-          clearable
-          filterable
-          placeholder="请选择"
-          size="mini"
-          v-model="searchform.changeStatus"
-        >
-          <el-option
-            :key="index"
-            :label="item.detailName"
-            :value="item.id"
-            v-for="(item, index) in ifBg"
-          ></el-option>
-        </el-select>
-      </el-form-item>
+      <!--<el-form-item-->
+      <!--label="是否变更:"-->
+      <!--&gt;-->
+      <!--<el-select-->
+      <!--clearable-->
+      <!--filterable-->
+      <!--placeholder="请选择"-->
+      <!--size="mini"-->
+      <!--v-model="searchform.changeStatus"-->
+      <!--&gt;-->
+      <!--<el-option-->
+      <!--:key="index"-->
+      <!--:label="item.detailName"-->
+      <!--:value="item.id"-->
+      <!--v-for="(item, index) in ifBg"-->
+      <!--&gt;</el-option>-->
+      <!--</el-select>-->
+      <!--</el-form-item>-->
       <el-form-item
         label="是否导入清单:"
       >
@@ -227,23 +225,16 @@
           <el-button slot="append" icon="el-icon-search"  @click="selectPosition()"></el-button>
         </el-input>
       </el-form-item>
-          <el-form-item style="float:right">
-            <el-button @click="searchformReset" type="info" plain style="color:black;background:none;float:right; margin-right:20px;"><i class="el-icon-refresh-right"></i>重置</el-button>
-            <el-button @click="getData" type="primary" style="float:right;margin-right:5px;" plain><i class="el-icon-search"></i>查询</el-button>
-            <el-button @click="exportdata" type="primary" style="float:right;margin-right:5px;" plain><i class="el-icon-upload2"></i>导出</el-button>
-          </el-form-item>
-        </el-form>
-        </el-menu-item-group>
-      </el-submenu>
-    </el-menu>
-    <div style="color:red;">
-      总计{{titleInfo.mapTotal.total}}个合同（其中不含分包{{titleInfo.mapNotSub['COUNTNOTSUB']}}个，系统内分包{{titleInfo.mapSystemSub['COUNTSYSTEMSUB']}}个，集团内分包{{titleInfo.mapGroupSub['COUNTGROUPSUB']}}个）；
-      我方份额总计{{titleInfo.mapTotal.totalAmount}}亿元（其中不含分包{{titleInfo.mapNotSub['SUMNOTSUB']}}亿元，系统内分包{{titleInfo.mapSystemSub['SUMSYSTEMSUB']}}亿元，集团内分包{{titleInfo.mapGroupSub['SUMGROUPSUB']}}亿元
-    </div>
+      <el-form-item style="float:right">
+        <el-button @click="searchformReset" type="info" plain style="color:black;background:none;float:right; margin-right:20px;"><i class="el-icon-refresh-right"></i>重置</el-button>
+        <el-button @click="getData" type="primary" style="float:right;margin-right:5px;" plain><i class="el-icon-search"></i>查询</el-button>
+        <el-button @click="exportdata" type="primary" style="float:right;margin-right:5px;" plain><i class="el-icon-upload2"></i>导出</el-button>
+      </el-form-item>
+    </el-form>
     <div style="margin-top: 10px">
       <el-table
-        :max-height="$tableHeight-100"
-        :height="$tableHeight-100"
+        :max-height="$tableHeight-130"
+        :height="$tableHeight-130"
         class=""
         :data="page.records"
         :header-cell-style="{'text-align': 'center','background-color': 'whitesmoke',}"
@@ -303,7 +294,7 @@
         <el-table-column
           :width="150"
           align="center"
-          label="我方份额"
+          label="初始我方份额"
           prop="ourAmount"
           show-overflow-tooltip
         >
@@ -384,14 +375,14 @@
           show-overflow-tooltip
         >
         </el-table-column>
-        <el-table-column
-          :width="300"
-          align="center"
-          label="共同承揽单位已阅状态"
-          prop="createOrgName"
-          show-overflow-tooltip
-        >
-        </el-table-column>
+        <!--<el-table-column-->
+        <!--:width="300"-->
+        <!--align="center"-->
+        <!--label="共同承揽单位已阅状态"-->
+        <!--prop="createOrgName"-->
+        <!--show-overflow-tooltip-->
+        <!--&gt;-->
+        <!--</el-table-column>-->
         <el-table-column
           :width="300"
           align="center"
@@ -462,14 +453,14 @@
           show-overflow-tooltip
         >
           <!--<template slot="header" slot-scope="scope">-->
-            <!--<span>版本标识</span>-->
-            <!--<div>-->
-              <!--<el-input-->
-                <!--class="list-search-picker"-->
-                <!--style=" width: 100%"-->
-                <!--v-model="searchFrom.version"-->
-              <!--/>-->
-            <!--</div>-->
+          <!--<span>版本标识</span>-->
+          <!--<div>-->
+          <!--<el-input-->
+          <!--class="list-search-picker"-->
+          <!--style=" width: 100%"-->
+          <!--v-model="searchFrom.version"-->
+          <!--/>-->
+          <!--</div>-->
           <!--</template>-->
         </el-table-column>
         <el-table-column
@@ -525,14 +516,14 @@
         contractInfoId:"",//合同id
         treeStatas: false,
         DwVisible:false,//选择单位弹框状态
-        titleInfo:{},
         page: {current: 1, size: 20, total: 0, records: []},
         searchform: {
           current: 1,
           size: 20,
           changeStatus:'0',
           projectNatureSecondId:'',
-          enginTypeSecondId:''
+          enginTypeSecondId:'',
+          flowStatus:'pass'
         },
         moneyform:{
           contractInfoAdjustLogList:[],
@@ -547,17 +538,21 @@
             detailName:'全部'
           },
           {
-            id:'1',
-            detailName:'未提交'
+            id:'edit',
+            detailName:'草稿'
           },
           {
-            id:'2',
+            id:'check',
             detailName:'审核中'
           },
           {
-            id:'3',
+            id:'pass',
             detailName:'审核通过'
-          }
+          },
+          {
+            id:'reject',
+            detailName:'审核退回'
+          },
         ],//项目状态列表
         ifBg:[
           {
@@ -576,10 +571,6 @@
       this.$store.dispatch('getCategory', {name: 'projectDomainType', id: '238a917eb2b111e9a1746778b5c1167e'});
       this.$store.dispatch('getCategory', {name: 'emergingMarket', id: '33de2e063b094bdf980c77ac7284eff3'});
       this.$store.dispatch('getCategory', {name: 'projectNature', id: '99239d3a143947498a5ec896eaba4a72'});
-      // 查询数据
-      this.$http.post("/api/contract/contract/ContractInfo/list/getContractInformation",).then((res)=>{
-        this.titleInfo = res.data.data
-      });
     },
     computed: {
       projectDomainType() {
@@ -637,24 +628,29 @@
         return url;
       },
       //打开单位弹框
-      addDw(type,list){
+      addDw(type,list,ifChek){
         this.DwVisible = true;
         this.$nextTick(() => {
-          this.$refs.infoDw.init(type,list);
-      })
+          this.$refs.infoDw.init(type,list,ifChek);
+        })
       },
       //获取单位的值
       getDwInfo(data){
         console.log(data);
-        var id=[],name=[];
-        if(data){
-          data.forEach((item)=>{
-            id.push(item.id);
-          name.push(item.detailName);
-        })
+        if(data.type=="填报单位"){
+          this.searchform.createOrgName=data.name;
+        }else if(data.type=="主推单位"){
+          var id=[],name=[];
+          if(data){
+            data.forEach((item)=>{
+              id.push(item.id);
+              name.push(item.detailName);
+            })
+          }
+          this.searchform.contractMianOrg=id.join(",");
+          this.searchform.path=name.join(",");
         }
-        this.searchform.contractMianOrg=id.join(",");
-        this.searchform.path=name.join(",");
+
         this.DwVisible=false;
       },
       //获取项目地点的值
@@ -671,7 +667,7 @@
         console.log(this.positionIndex);
         this.$nextTick(() => {
           this.$refs.addOrUpdate.init()
-      })
+        })
       },
       //工程类别二级
       getTwo(id) {
@@ -680,11 +676,11 @@
         if(id!=''){
           this.projectDomainType.find(
             (item) => {
-            if (item.id == id) {
-            this.xqprojectType = item.children;
-          }
-        }
-        )
+              if (item.id == id) {
+                this.xqprojectType = item.children;
+              }
+            }
+          )
         }
       },
       //项目性质二级
@@ -694,12 +690,12 @@
         if(id!=''){
           this.projectNature.find(
             (item)=>{
-            if (item.id == id) {
-            this.searchform.projectNatureFirstName = item.detailName;
-            this.projectNatureTwo = item.children;
-          }
-        }
-        )
+              if (item.id == id) {
+                this.searchform.projectNatureFirstName = item.detailName;
+                this.projectNatureTwo = item.children;
+              }
+            }
+          )
         }
       },
       exportdata() {
@@ -728,7 +724,7 @@
               '<th>新兴市场(二级)</th>\n' +
               '<th>合同签订日期</th>\n' +
               '<th>地点</th>\n' +
-              '<th>共同承揽单位已阅状态</th>\n' +
+
               '<th>签约单位(使用资质单位)</th>\n' +
               '<th>建设单位</th>\n' +
               '<th>填报单位</th>\n' +
@@ -744,7 +740,7 @@
                 'ourAmount','valueAddedTax','contractBuiltArea','lineLength',
                 'ourAmountSupply','enginTypeFirstName','marketFirstName','marketSecondName',
                 'contractSignTime','placeName','createOrgName','signOrgName',
-                'constructionOrg','createOrgName','createTime','createUserName',
+                'constructionOrg','createTime','createUserName',
                 'pushOrgName','bidTime','version','isOpenBid',
               ],
               tdstrFuc:{
@@ -812,18 +808,18 @@
       },
       // 查询
       getData() {
-        if(this.searchform.createTime){
-          this.searchform.beginTime=this.searchform.createTime[0];
-          this.searchform.stopTime=this.searchform.createTime[1];
-        }
+        // if(this.searchform.createTime){
+        //   this.searchform.beginTime=this.searchform.createTime[0];
+        //   this.searchform.stopTime=this.searchform.createTime[1];
+        // }
         this.$http
           .post(
             "/api/contract/contract/ContractInfo/list/loadPageDataForContractInfoAdjust",
             this.searchform
           )
           .then((res)=>{
-          this.page = res.data.data;
-      });
+            this.page = res.data.data;
+          });
       },
 
     },
