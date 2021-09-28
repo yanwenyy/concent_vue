c0781fceda6c692eda8e62c23f7fd24a5a3fbb31
GaoDeXin <65593527+q809041405@users.noreply.github.com>
2021-09-28 11:04:59 +0800

    fix 投标信息登记 附件上传优化

--- concent_vue/src/views/manage/bid_info/detail.vue
+++ concent_vue/src/views/manage/bid_info/detail.vue
@@ -786,13 +786,25 @@
           <el-row>
             <p class="detail-title">
               <span>附件: </span>
-              <el-button
+              <!-- <el-button
                 v-show="p.actpoint != 'look' && p.actpoint !== 'searchLook'&& p.actpoint !== 'task'"
                 size="small"
                 type="primary"
                 @click="openFileUp('/api/contract/topInfo/CommonFiles/bidInfo/01/uploadFile','bidInfo_01')">
                 点击上传
-              </el-button>
+              </el-button> -->
+              <el-upload
+                :headers="{'Authorization':Authorization}"
+                v-show="p.actpoint !== 'look'&&p.actpoint !== 'task'"
+                class="upload-demo detailUpload detatil-flie-btn"
+                :action="'/api/contract/topInfo/CommonFiles/bidInfo/01/uploadFile'"
+                :on-change="( file, fileList)=>{uploadPorgress( file, fileList,detailform.bidInfo_01)}"
+                :show-file-list="false"
+                :before-upload="beforeAvatarUpload"
+                multiple
+              >
+                <el-button size="small" type="primary">点击上传</el-button>
+              </el-upload>
             </p>
             <el-table
               :data="detailform.bidInfo_01"
@@ -843,7 +855,13 @@
                 width="100"
               >
               </el-table-column>
-
+              <el-table-column align="center" width="200" :resizable="false" label="上传进度" show-overflow-tooltip>
+                <template slot-scope="scope">
+                  <el-progress v-if="scope.row.progressFlag=='start'" :percentage="scope.row.loadProgress||0"></el-progress>
+                  <el-progress  v-if="scope.row.progressFlag=='fail'" :percentage="100" status="warning"></el-progress>
+                  <span v-if="scope.row.progressFlag=='stop'||scope.row.progressFlag==null">已上传</span>
+                </template>
+              </el-table-column>
               <el-table-column
                 align="center"
                 :resizable="false"
@@ -857,7 +875,7 @@
                   <el-link
                     v-if="p.actpoint !== 'look' && p.actpoint !== 'searchLook'&& p.actpoint !== 'task'"
                     :underline="false"
-                    @click="handleRemove1(scope.row, scope.$index)"
+                    @click="handleRemove(scope.row, scope.$index)"
                     type="warning"
                     >删除
                   </el-link>
@@ -1348,6 +1366,7 @@ export default {
       } 
     }
     return {
+      Authorization:sessionStorage.getItem("token"),
       companyMulStatus:false,//设计单位等多选列表状态
       uploadVisible:false,//上传附件组件状态
       maxMoney: 1000000,
@@ -1489,47 +1508,138 @@ export default {
       this.detailform[data.list]=this.detailform[data.list].concat(data.fileList);
       this.uploadVisible = false;
     },
-      //流程操作
-      operation(type){
-        var msg='',that=this;
-        this.$prompt('请输入审核意见', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-        }).then(({ value }) => {
-          if(type=='back'){
-            if(value==null||value==''){
-              this.$message.error('审核意见不能为空');
-              return false;
-            }
-          }else{
-            if(value==null||value==''){
-              value=that.examineReviewMsg;
-            }
+    //流程操作
+    operation(type){
+      var msg='',that=this;
+      this.$prompt('请输入审核意见', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+      }).then(({ value }) => {
+        if(type=='back'){
+          if(value==null||value==''){
+            this.$message.error('审核意见不能为空');
+            return false;
           }
-          this.p.task.remark=value;
-          this.$http
-            .post(
-              '/api/contract/topInfo/BidInfo/process/'+type,
-              JSON.stringify(this.p.task),
-              {useJson: true}
-            )
-            .then((res) => {
-              if (res.data.code === 200) {
-                this.$message({
-                  message: "操作成功",
-                  type: "success",
-                });
-                this.$router.back()
-              }
-            });
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '取消输入'
+        }else{
+          if(value==null||value==''){
+            value=that.examineReviewMsg;
+          }
+        }
+        this.p.task.remark=value;
+        this.$http
+          .post(
+            '/api/contract/topInfo/BidInfo/process/'+type,
+            JSON.stringify(this.p.task),
+            {useJson: true}
+          )
+          .then((res) => {
+            if (res.data.code === 200) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+              });
+              this.$router.back()
+            }
           });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        });
+      });
+
+    },
+    //判断附件大小
+    beforeAvatarUpload(file) {
+      var fileLimit=Number(this.fileLimit);
+      const isJPG = file.type === 'image/jpeg';
+      const isLt100M = file.size / (1024 * fileLimit) < fileLimit;
+
+      // if (!isJPG) {
+      //   this.$message.error('上传头像图片只能是 JPG 格式!');
+      // }
+      if (!isLt100M) {
+        this.$message.error('上传文件大小不能超过 '+fileLimit+'MB!');
+      }
+      // return isJPG && isLt2M;
+      return isLt100M;
+    },
+    //上传附件显示进度条
+    uploadPorgress(file, fileList,tableList){
+      // console.log(event, file, fileList,tableList);
+      // console.log(fileList)
+      const len=tableList.length;
+      if (file.status === 'ready') {
+        file.fileName=file.name;
+        file.fileSize=file.size;
+        // file.fileType=file.type;
+        file.progressFlag = 'start'; // 显示进度条
+
+        file.loadProgress=0;
+
+        tableList.push(file);
+        var that=this;
+        tableList.forEach((item,index)=>{
+
+          const interval = setInterval(() => {
+            if (item&&item.loadProgress >= 90) {
+              item.loadProgress = 90;
+              if(file.response&&item.fileName==file.response.data.fileName&&file.response.data.progressFlag=='stop'){
+                tableList[index]=file.response.data;
+                // console.log(index,'==>',tableList[index])
+                that.$set(tableList,index,tableList[index])
+                // console.log(tableList[index])
+              }
+
+              clearInterval(interval);
+              return
+            }
+            if(item.progressFlag == 'start'){
+              item.loadProgress += 20;//进度条进度
+              // that.$set(tableList[len],tableList[len])
+              that.$set(tableList,index,tableList[index])
+              // console.log(tableList[len].loadProgress)
+
+            }
+            if(file.response&&file.response.data.progressFlag=='fail'){
+              tableList[index].progressFlag='fail';
+              this.$set(tableList,tableList)
+            }
+          }, 600);
         });
 
-      },
+      }
+      if (file.response && file.response.code === 200) {
+        // console.log(tableList.length)
+        this.$message({
+          message: '上传成功',
+          type: 'success',
+          duration: 1000,
+          onClose: () => {
+            // const len=tableList.length;
+
+            file.response.data.progressFlag='stop';
+            tableList.forEach((item,index)=>{
+              if(item.fileName==file.response.data.fileName&&item.progressFlag!='stop'){
+                tableList[index]=file.response.data;
+                // console.log(index,'==>',tableList[index])
+                this.$set(tableList,index,tableList[index])
+                // console.log(tableList[index])
+              }
+            })
+            // tableList[len-1]=file.response.data;
+
+          }
+        })
+      }else if(file.response && file.response.code !== 200){
+        // tableList[len-1].progressFlag = 'fail';
+        file.response.data.progressFlag='fail';
+        this.$set(tableList,tableList)
+        this.$message.error(file.response.msg);
+      }
+
+      this.$forceUpdate();
+    },
     // 附件下载
     attachmentDownload(file){
       this.$handleDownload(file)
@@ -1647,17 +1757,17 @@ export default {
       this.BDCSVisible = false;
       this.key = this.key + 1;
     },
-    handleRemove(file, fileList) {
+    handleRemove(file,index) {
       this.$http
-        .post("/api/contract/topInfo/CommonFiles/list/delete", {
-          ids: [file.response.data.uuid],
-        })
+        .post(
+          "/api/contract/topInfo/CommonFiles/list/delete",
+          {ids:[file.uuid]},
+        )
         .then((res) => {
-          if (res.data.code === 200) {
-            this.detailform.bidInfo_01 = fileList;
-          }
-        });
-      console.log(this.fileList);
+        if (res.data.code === 200) {
+          this.detailform.bidInfo_01.splice(index,1);
+        }
+      });
     },
     //上传附件
     handleChange(response, file, fileList) {
